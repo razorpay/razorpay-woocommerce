3167a9548d78f610289a0c9e9a4c5d3a55cb06ed
Abhay Rana <nemo@razorpay.com>
2016-01-19 12:37:57 +0530

    Version bump

--- razorpay-payments.php
+++ razorpay-payments.php
@@ -3,7 +3,7 @@
 Plugin Name: WooCommerce Razorpay Payments
 Plugin URI: https://razorpay.com
 Description: Razorpay Payment Gateway Integration for WooCommerce
-Version: 1.2.2
+Version: 1.2.4
 Author: Razorpay
 Author URI: https://razorpay.com
 */
