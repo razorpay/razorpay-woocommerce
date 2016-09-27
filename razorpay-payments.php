c29c709e4db6b8bbe7b0f5298300bf7621e8ac5d
Vivek Kumar <viveklakhera@gmail.com>
2016-09-27 18:52:22 +0530

    Add server verification for manual capture

--- razorpay-payments.php
+++ razorpay-payments.php
@@ -184,7 +184,7 @@ Please wait while we are processing your payment.
         elem.removeAttribute('disabled');
       }
       else {
-        elem.setAttribute('disabled', state); 
+        elem.setAttribute('disabled', state);
       }
     };
 
@@ -254,7 +254,7 @@ EOT;
         {
             global $woocommerce;
 
-            if (isset($_REQUEST['merchant_order_id']) and isset($_REQUEST['razorpay_payment_id']))
+            if ((empty($_REQUEST['merchant_order_id']) === false) and (empty($_REQUEST['razorpay_payment_id']) === false))
             {
                 $order_id = $_REQUEST['merchant_order_id'];
                 $razorpay_payment_id = $_REQUEST['razorpay_payment_id'];
@@ -267,71 +267,79 @@ EOT;
                 $success = false;
                 $error = "";
 
-                if ($this->payment_action === 'authorize')
+                $ch = curl_init();
+
+                curl_setopt($ch,CURLOPT_USERPWD, $key_id . ":" . $key_secret);
+                curl_setopt($ch,CURLOPT_TIMEOUT, 60);
+
+                try
                 {
-                    $success = true;
-                }
-                else
-                {
-                    try
+                    if ($this->payment_action === 'authorize')
+                    {
+                        $url = self::BASE_URL . self::API_VERSION . "/payments/{$razorpay_payment_id}";
+
+                        curl_setopt($ch,CURLOPT_URL, $url);
+
+                        $status = 'authorized';
+                    }
+                    else
                     {
                         $url = self::BASE_URL . self::API_VERSION . "/payments/{$razorpay_payment_id}/capture";
                         $fields_string="amount={$amount}";
 
-                        //cURL Request
-                        $ch = curl_init();
-
-                        //set the url, number of POST vars, POST data
                         curl_setopt($ch,CURLOPT_URL, $url);
-                        curl_setopt($ch,CURLOPT_USERPWD, $key_id . ":" . $key_secret);
-                        curl_setopt($ch,CURLOPT_TIMEOUT, 60);
                         curl_setopt($ch,CURLOPT_POST, 1);
                         curl_setopt($ch,CURLOPT_POSTFIELDS, $fields_string);
-                        curl_setopt($ch,CURLOPT_RETURNTRANSFER, true);
-                        curl_setopt($ch,CURLOPT_SSL_VERIFYPEER, true);
-                        curl_setopt($ch,CURLOPT_CAINFO, plugin_dir_path(__FILE__) . 'ca-bundle.crt');
 
-                        //execute post
-                        $result = curl_exec($ch);
-                        $http_status = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+                        $status = 'captured';
+                    }
 
+                    curl_setopt($ch,CURLOPT_RETURNTRANSFER, true);
+                    curl_setopt($ch,CURLOPT_SSL_VERIFYPEER, true);
+                    curl_setopt($ch,CURLOPT_CAINFO, plugin_dir_path(__FILE__) . 'ca-bundle.crt');
 
-                        if ($result === false)
+                    //execute post
+                    $result = curl_exec($ch);
+                    $http_status = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+
+                    if ($result === false)
+                    {
+                        $success = false;
+                        $error = 'Curl error: ' . curl_error($ch);
+                    }
+                    else
+                    {
+                        $response_array = json_decode($result, true);
+                        //Check success response
+                        if (($http_status === 200) and
+                            (isset($response_array['error']) === false) and
+                            ($response_array['status'] === $status))
                         {
-                            $success = false;
-                            $error = 'Curl error: ' . curl_error($ch);
+                            $success = true;
                         }
                         else
                         {
-                            $response_array = json_decode($result, true);
-                            //Check success response
-                            if ($http_status === 200 and isset($response_array['error']) === false)
+                            $success = false;
+
+                            if (empty($response_array['error']['code']) === false)
                             {
-                                $success = true;
+                                $error = $response_array['error']['code'].": ".$response_array['error']['description'];
                             }
                             else
                             {
-                                $success = false;
-
-                                if (!empty($response_array['error']['code']))
-                                {
-                                    $error = $response_array['error']['code'].": ".$response_array['error']['description'];
-                                }
-                                else
-                                {
-                                    $error = "RAZORPAY_ERROR: Invalid Response <br/>".$result;
-                                }
+                                $error = "RAZORPAY_ERROR: Invalid Response <br/>".$result;
                             }
                         }
-                        //close connection
-                        curl_close($ch);
-                    }
-                    catch (Exception $e)
-                    {
-                        $success = false;
-                        $error ="WOOCOMMERCE_ERROR: Request to Razorpay Failed";
                     }
                 }
+                catch (Exception $e)
+                {
+                    $success = false;
+                    $error ="WOOCOMMERCE_ERROR: Request to Razorpay Failed";
+                }
+
+                //close connection
+                curl_close($ch);
 
                 if ($success === true)
                 {
