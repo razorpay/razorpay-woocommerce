c71cf92f26d50c963ad69cb0afda7219bab9c2ef
mayankamencherla <mayank.amencherla@razorpay.com>
2016-10-22 12:23:09 +0530

    Order creation now happens over multiple functions, and the code is more readable

--- razorpay-payments.php
+++ razorpay-payments.php
@@ -10,8 +10,6 @@ Author URI: https://razorpay.com
 
 require_once __DIR__.'/razorpay-sdk/Razorpay.php';
 use Razorpay\Api\Api;
-use Razorpay\Api\Order;
-use Razorpay\Api\Request;
 
 add_action('plugins_loaded', 'woocommerce_razorpay_init', 0);
 
@@ -143,8 +141,6 @@ function woocommerce_razorpay_init()
         {
             global $woocommerce;
 
-            // currently in the form only razorpay payment ID is there. But with the addition of order ID and signature, this should be straight forward
-
             $order = new WC_Order($order_id);
 
             $redirect_url = get_site_url() . '/?wc-api=' . get_class($this);
@@ -153,26 +149,8 @@ function woocommerce_razorpay_init()
 
             $api = new Api($this->key_id, $this->key_secret);
 
-            switch($this->payment_action)
-            {
-                case 'authorize':
-                    $data = array(
-                      'receipt' => $order_id,
-                      'amount' => (int) ($order->order_total * 100),
-                      'currency' => get_woocommerce_currency(),
-                      'payment_capture' => 0
-                    );    
-                    break;
-
-                default:
-                    $data = array(
-                      'receipt' => $order_id,
-                      'amount' => (int) ($order->order_total * 100),
-                      'currency' => get_woocommerce_currency(),
-                      'payment_capture' => 1
-                    );
-                    break;
-            }
+            // Calls the helper function to create order data
+            $data = $this->get_order_creation_data($order_id);
             
             $razorpay_order = $api->order->create($data);
             
@@ -196,6 +174,49 @@ function woocommerce_razorpay_init()
             );
 
             $json = json_encode($razorpay_args);
+
+            $html = $this->generate_order_form($redirect_url,$json);
+
+            return $html;
+        }
+
+
+        /**
+         * Creates order data
+        **/
+        function get_order_creation_data($order_id)
+        {
+            $order = new WC_Order($order_id);
+            
+            switch($this->payment_action)
+            {
+                case 'authorize':
+                    $data = array(
+                      'receipt' => $order_id,
+                      'amount' => (int) ($order->order_total * 100),
+                      'currency' => get_woocommerce_currency(),
+                      'payment_capture' => 0
+                    );    
+                    break;
+
+                default:
+                    $data = array(
+                      'receipt' => $order_id,
+                      'amount' => (int) ($order->order_total * 100),
+                      'currency' => get_woocommerce_currency(),
+                      'payment_capture' => 1
+                    );
+                    break;
+            }
+
+            return $data;
+        }
+
+        /**
+         * Generates the order form
+        **/
+        function generate_order_form($redirect_url, $json)
+        {
 
             $html = <<<EOT
 <script src="{$this->liveurl}"></script>
