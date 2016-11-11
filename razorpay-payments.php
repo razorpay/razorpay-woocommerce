0151696dde22caeb0056d74be10022ad9496fab1
mayankamencherla <mayank.amencherla@razorpay.com>
2016-11-11 13:21:09 +0530

    Removed unnecessary includes

--- razorpay-payments.php
+++ razorpay-payments.php
@@ -13,8 +13,6 @@ require_once __DIR__.'/includes/razorpay-webhook.php';
 require_once __DIR__.'/razorpay-sdk/Razorpay.php';
 use Razorpay\Api\Api;
 
-require_once ABSPATH . WPINC . '/compat.php';
-
 add_action('plugins_loaded', 'woocommerce_razorpay_init', 0);
 add_action('admin_post_nopriv_rzp_webhook', 'razorpay_webhook_init'); // second - admin_post_nopriv
 add_action('admin_post_rzp_webhook', 'razorpay_webhook_init'); // second - admin_post_nopriv
