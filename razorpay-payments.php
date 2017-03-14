a0756684ce3173e17b95e1cc087eabbe0ce7b5fe
mayankamencherla <mayank.amencherla@razorpay.com>
2017-03-14 15:13:00 +0530

    Minor improvement

--- razorpay-payments.php
+++ razorpay-payments.php
@@ -171,8 +171,6 @@ function woocommerce_razorpay_init()
                 echo "RAZORPAY ERROR: Api could not be reached";
             }
 
-            die($order->order_total);
-
             $razorpay_args = array(
               'key'         => $this->key_id,
               'name'        => get_bloginfo('name'),
