78923bb65208fb696f5f8c66bd4a146df5334511
mayankamencherla <mayank.amencherla@razorpay.com>
2017-10-23 18:58:54 +0530

    [upi sbi] Removed  declaration in subscriptions child class

--- razorpay-subscriptions.php
+++ razorpay-subscriptions.php
@@ -72,12 +72,6 @@ function woocommerce_razorpay_subscriptions_init()
         );
 
         /**
-         * Icon URL, set in constructor
-         * @var string
-         */
-        public $icon;
-
-        /**
          * Instance of the class RZP_subscriptions found in __DIR__ . 'includes/razorpay-subscriptions'
          * It is used to make all subscriptions related API calls to the Razorpay's API
          * @var RZP_Subscriptions
