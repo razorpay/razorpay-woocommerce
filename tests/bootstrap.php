2233ed1b3f6cc10572fb26dcca24d45d128b07d1
tessie <tessyjoseph1992@gmail.com>
2017-11-16 08:23:30 +0530

    Added test for RazoryP_Webhook to verify that amount is returned as interger

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -19,12 +19,27 @@ if ( ! file_exists( $_tests_dir . '/includes/functions.php' ) ) {
 require_once $_tests_dir . '/includes/functions.php';
 
 /**
- * Manually load the plugin being tested.
+ * Manually load the plugin being tested. And also woocommerce pligin
  */
 function _manually_load_plugin() {
 	require dirname( dirname( __FILE__ ) ) . '/woo-razorpay.php';
+	require dirname( dirname( __FILE__ ) ) . '../../woocommerce/woocommerce.php';
 }
+
+function is_woocommerce_active() {
+   return true;
+}
+
+function woothemes_queue_update($file, $file_id, $product_id) {
+   return true;
+}
+
 tests_add_filter( 'muplugins_loaded', '_manually_load_plugin' );
 
 // Start up the WP testing environment.
 require $_tests_dir . '/includes/bootstrap.php';
+
+$wc_tests_framework_base_dir = dirname( dirname( __FILE__ ) ) . '../../woocommerce/tests/framework/';
+require_once( $wc_tests_framework_base_dir . 'helpers/class-wc-helper-product.php'  );
+require_once( $wc_tests_framework_base_dir . 'helpers/class-wc-helper-shipping.php'  );
+require_once( $wc_tests_framework_base_dir . 'helpers/class-wc-helper-order.php'  );
