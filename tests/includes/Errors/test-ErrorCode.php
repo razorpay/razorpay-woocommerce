b25882d433f7b99189116dcc86dcd16d08fd9f46
tessie <tessyjoseph1992@gmail.com>
2017-11-17 13:28:33 +0530

    Following coding guide lines

--- tests/includes/Errors/test-ErrorCode.php
+++ tests/includes/Errors/test-ErrorCode.php
@@ -1,4 +1,3 @@
-
 <?php
 /**
  * Class ErrorCodeTest
@@ -11,19 +10,19 @@
 use Razorpay\Woocommerce\Errors as WooErrors;
 
 class ErrorCodeTest extends WP_UnitTestCase
-	{
-		
+{
+
 	function test_constants()
-		{
+	{
 		$this->assertEquals(WooErrors\ErrorCode::INVALID_CURRENCY_ERROR_CODE, 'INVALID_CURRENCY_ERROR');
 		$this->assertEquals(WooErrors\ErrorCode::WOOCS_CURRENCY_MISSING_ERROR_CODE, 'WOOCS_CURRENCY_MISSING_ERROR');
 		$this->assertEquals(WooErrors\ErrorCode::WOOCS_MISSING_ERROR_CODE, 'WOOCS_MISSING_ERROR');
-		}
+	}
 
-	function test_error_messages()
-		{
+	function test_errorMessages()
+	{
 		$this->assertEquals(WooErrors\ErrorCode::WOOCS_MISSING_ERROR_MESSAGE, 'The WooCommerce Currency Switcher plugin is missing.');
 		$this->assertEquals(WooErrors\ErrorCode::INVALID_CURRENCY_ERROR_MESSAGE, 'The selected currency is invalid.');
 		$this->assertEquals(WooErrors\ErrorCode::WOOCS_CURRENCY_MISSING_ERROR_MESSAGE, 'Woocommerce Currency Switcher plugin is not configured with INR correctly');
-		}
 	}
+}
--- tests/includes/test-razorpay-webhook.php
+++ tests/includes/test-razorpay-webhook.php
@@ -4,18 +4,19 @@
  *
  * @package Woo_Razorpay
  */
+
 class TestRZP_Webhook extends WP_UnitTestCase
 {
-    
+
     function setup()
     {
         $this->rzpWebhook = new RZP_Webhook();
     }
-    
+
     function test_getOrderAmountAsInteger()
     {
         $order = WC_Helper_Order::create_order();
         $this->assertEquals($this->rzpWebhook->getOrderAmountAsInteger($order), 5000);
     }
 }
- 
+
