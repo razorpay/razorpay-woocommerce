444e21285edce07120428ca5d9b4da5487a61e49
tessie <tessyjoseph1992@gmail.com>
2018-03-29 11:09:57 +0530

    load woocommerce

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -23,6 +23,8 @@ require_once $_tests_dir . '/includes/functions.php';
  */
 function _manually_load_plugin() {
 	require dirname( dirname( __FILE__ ) ) . '/woo-razorpay.php';
+    require dirname( dirname( __FILE__ ) ) . '/wp-content/plugins/woocommerce/woocommerce.php';
+
 }
 tests_add_filter( 'muplugins_loaded', '_manually_load_plugin' );
 
