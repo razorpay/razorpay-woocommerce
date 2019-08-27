6fa2cd56182ecc52a625aa18f22cfbd3589110dd
prem chand <prem.techchef@razorpay.com>
2019-08-27 17:50:03 +0530

    Add webhook for subscription cancel

--- woo-razorpay.php
+++ woo-razorpay.php
@@ -3,8 +3,8 @@
  * Plugin Name: Razorpay for WooCommerce
  * Plugin URI: https://razorpay.com
  * Description: Razorpay Payment Gateway Integration for WooCommerce
- * Version: 2.1.0
- * Stable tag: 2.1.0
+ * Version: 2.2.0
+ * Stable tag: 2.2.0
  * Author: Team Razorpay
  * WC tested up to: 3.6.5
  * Author URI: https://razorpay.com
