346c12b96ee26242ce48ba098af7f1bb61fc331a
Anurag Bandyopadhyay <anurag.bandyopadhyay@razorpay.com>
2022-07-27 12:53:18 +0530

    Added nonce user logged out

--- includes/api/api.php
+++ includes/api/api.php
@@ -207,6 +207,14 @@ function addMagicCheckoutSettingFields(&$defaultFormFields)
 
 }
 
+//To handle rest cookies invalid issue 
+add_filter("nonce_user_logged_out", function ($uid, $action) {
+    if ($uid === 0 && $action === 'wp_rest') {
+      return null;
+    }
+    return $uid;
+}, 10, 2);
+
 add_filter( 'rest_authentication_errors', function( $maybe_error ) {
     return true;
 });
