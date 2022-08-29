4e77fa11903db57cc3561d73706f351bbeb7c851
angbpy <105670258+angbpy@users.noreply.github.com>
2022-08-29 13:25:00 +0530

    Script endpoint change (#335)
    * Script endpoint change
    
    * Script endpoint fix

--- woo-razorpay.php
+++ woo-razorpay.php
@@ -2099,7 +2099,7 @@ function razorpay_webhook_init()
 }
 
 define('RZP_PATH', plugin_dir_path( __FILE__ ));
-define('RZP_CHECKOUTJS_URL', 'https://checkout.razorpay.com/v1/checkout.js');
+define('RZP_CHECKOUTJS_URL', 'https://checkout.razorpay.com/v1/checkout-1cc.js');
 define('RZP_1CC_CSS_SCRIPT', 'RZP_1CC_CSS_SCRIPT');
 
 
