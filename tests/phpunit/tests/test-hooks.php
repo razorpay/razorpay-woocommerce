a5d3f651bf21e258cc08d19f48bfa961fb8235fc
Garima Prachi <garima.prachi@razorpay.com>
2022-11-14 17:42:27 +0530

    Unit test cases for init_hooks() and init_formfields()

--- tests/phpunit/tests/test-hooks.php
+++ tests/phpunit/tests/test-hooks.php
@@ -12,5 +12,21 @@ class Test_Hooks extends WP_UnitTestCase
     public function testRegisteredHooks(){
         $this->assertTrue(Util::has_action('woocommerce_update_options_payment_gateways_'.$this->razorpayTests->id,
             $this->razorpayTests, 'pluginInstrumentation'));
+            
+            $this->assertTrue(Util::has_action('woocommerce_update_options_payment_gateways_'.$this->razorpayTests->id,
+            $this->razorpayTests, 'process_admin_options'));
+           
+            $this->assertTrue(Util::has_action('woocommerce_update_options_payment_gateways_'.$this->razorpayTests->id,
+            $this->razorpayTests, 'autoEnableWebhook'));
+            
+            $this->assertTrue(Util::has_action('woocommerce_update_options_payment_gateways_'.$this->razorpayTests->id,
+            $this->razorpayTests, 'addAdminCheckoutSettingsAlert'));
+            
+            $this->assertTrue(Util::has_action('woocommerce_receipt_'.$this->razorpayTests->id,
+            $this->razorpayTests, 'receipt_page'));
+
+            $this->assertTrue(Util::has_action('woocommerce_api_'.$this->razorpayTests->id,
+            $this->razorpayTests, 'check_razorpay_response'));
+
     }
 }
