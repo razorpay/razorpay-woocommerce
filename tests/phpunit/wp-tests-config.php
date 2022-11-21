012293f9a31fc7449e2f9ea1d24e75a32b0d2680
abdulwahidsharief <abdul.sharief@razorpay.com>
2022-11-21 15:02:02 +0530

    db name and port fixed

--- tests/phpunit/wp-tests-config.php
+++ tests/phpunit/wp-tests-config.php
@@ -11,17 +11,17 @@ define( 'WP_DEBUG', false );
 // WARNING WARNING WARNING!
 // tests DROPS ALL TABLES in the database. DO NOT use a production database
 
-define( 'DB_NAME', 'wptest' );
+define( 'DB_NAME', 'wptt_tests' );
 define( 'DB_USER', 'root' );
 define( 'DB_PASSWORD', 'root' );
-define( 'DB_HOST', '127.0.0.1:8889' );
+define( 'DB_HOST', '127.0.0.1' );
 define( 'DB_CHARSET', 'utf8' );
 define( 'DB_COLLATE', '' );
 
-$table_prefix = 'wptests_'; // Only numbers, letters, and underscores please!
+$table_prefix = 'wptt_tests'; // Only numbers, letters, and underscores please!
 
 define( 'WP_TESTS_DOMAIN', '127.0.0.1' );
-define( 'WP_TESTS_EMAIL', 'admin@example.org' );
+define( 'WP_TESTS_EMAIL', 'admin@wp.test' );
 define( 'WP_TESTS_TITLE', 'Test Blog' );
 
 define( 'WP_PHP_BINARY', 'php' );
