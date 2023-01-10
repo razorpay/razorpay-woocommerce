1f774aa53c3cbd3042efeffc875cc9c28249ca35
abdulwahidsharief <abdul.sharief@razorpay.com>
2023-01-10 09:26:43 +0530

    test plugin instrumentation class

--- tests/phpunit/tests/test-instrumentation.php
+++ tests/phpunit/tests/test-instrumentation.php
@@ -18,19 +18,36 @@ class Test_Instrumentation extends WP_UnitTestCase
 
     public function testHooks()
     {
-        $this->assertSame(10, has_action('activate_' . basename(PLUGIN_DIR) .'/woo-razorpay.php', 'razorpayPluginActivated'));
-        $this->assertSame(10, has_action('deactivate_' . basename(PLUGIN_DIR) .'/woo-razorpay.php', 'razorpayPluginDeactivated'));
+        $this->assertSame(10, has_action('activate_' . basename(PLUGIN_DIR) .'/woo-razorpay.php',
+            'razorpayPluginActivated'));
+        $this->assertSame(10, has_action('deactivate_' . basename(PLUGIN_DIR) .'/woo-razorpay.php',
+            'razorpayPluginDeactivated'));
         $this->assertSame(10, has_action('upgrader_process_complete', 'razorpayPluginUpgraded'));
     }
 
     public function testInstrumentationSegment()
     {
         $this->rzpInstrumentation->api = new MockApi('key_id', 'key_secret');
-
-        $response = $this->rzpInstrumentation->rzpTrackSegment('event',
-            ['key' => 'value']
-        );
-
+        $response = $this->rzpInstrumentation->rzpTrackSegment('testing', ['key' => 'value']);
         $this->assertSame('success', $response['status']);
+    }
+
+    public function testInstrumentationDataLake()
+    {
+        $response = $this->rzpInstrumentation->rzpTrackDataLake('testing', ['key' => 'value']);
+        $this->assertSame(200, $response['response']['code']);
+    }
+
+    public function testGetDefaultPropertiesWithTimeStamp()
+    {
+        $response = $this->rzpInstrumentation->getDefaultProperties();
+
+        $this->assertSame('WordPress', $response['platform']);
+        $this->assertNotNull($response['platform_version']);
+        $this->assertNotNull($response['woocommerce_version']);
+        $this->assertSame('Razorpay for WooCommerce', $response['plugin_name']);
+        $this->assertNotNull($response['plugin_version']);
+        $this->assertNotNull($response['unique_id']);
+        $this->assertNotNull($response['event_timestamp']);
     }
 }
