022dbcb83c598d0b8f2b4034af2a41556d331b3a
Garima Prachi <garima.prachi@razorpay.com>
2023-01-27 18:20:22 +0530

    Ut for newUser and verifyOrder

--- tests/phpunit/tests/test-order-methods.php
+++ tests/phpunit/tests/test-order-methods.php
@@ -5,6 +5,7 @@
 
 require_once __DIR__ . '/../mockfactory/MockApi.php';
 require_once __DIR__ . '/../mockfactory/Request.php';
+require_once __DIR__ . '/../mockfactory/Order.php';
 
 use Razorpay\MockApi\MockApi;
 
@@ -24,105 +25,80 @@ class Test_OrderMethods extends WP_UnitTestCase
         $_POST = array();
     }
 
-    public function testUpdateOrder()
-    {
-        global $woocommerce;
-
-        $order = wc_create_order();
-
-        $cart = $woocommerce->cart;
-
-        $woocommerce->cart->add_to_cart(14,25);
-
-        $order->set_payment_method('cod');
-
-        $this->instance->updateOrder($order, true, "", 'razorpay_order_id');
-
-        $this->assertSame('processing', $order->get_status() );
-
-        $this->instance->updateOrder($order, false, "Error message", 'razorpay_order_id');
-
-        $reflection = new ReflectionProperty('WC_Razorpay', 'msg');
-        $reflection->setAccessible(true);
-        $msg = $reflection->getValue($this->instance);
-
-        $this->assertSame($msg['class'], 'error');
-
-        $this->assertSame($msg['message'], "Error message");
-    
-        $this->assertSame('failed', $order->get_status());
-    }
-
-    public function testupdateUserAddressInfo()
+    public function testnewUserAccount()
     {
         $order = wc_create_order();
 
-        $order = wc_create_order(array('customer_id' => 189));
+        $shippingAddress = (object)array('name' => 'ABC');
+        $shippingAddress = (object)array('line1'=> 'SJR Cyber Laskar');
+        $shippingAddress = (object)array('line2' => 'Hosur Rd, Adugodi');
+        $shippingAddress = (object)array('city' => 'Bengaluru');
+        $shippingAddress = (object)array('country' => strtoupper('India'));
+        $shippingAddress = (object)array('postcode' => '560030');
+        $shippingAddress = (object)array('state' =>strtoupper('Karnataka'));
 
-        $shippingAddress = [];
-
-        $shippingAddress['first_name'] = 'ABC';
-        $shippingAddress['address_1'] = 'SJR Cyber Laskar';
-        $shippingAddress['address_2'] = 'Hosur Rd, Adugodi';
-        $shippingAddress['city'] = 'Bengaluru';
-        $shippingAddress['country'] = strtoupper('India');
-        $shippingAddress['postcode'] = '560030';
-        $shippingAddress['email'] = 'abc.xyz@razorpay.com';
-        $shippingAddress['phone'] = '18001231272';
-
-        $shippingState = strtoupper('Karnataka');
+        $shippingState = strtoupper($shippingAddress->state);
         $shippingStateName = str_replace(" ", '', $shippingState);
         $shippingStateCode = getWcStateCodeFromName($shippingStateName);
-
-        $order->set_shipping_state($shippingStateCode);
-
-        $this->instance->updateUserAddressInfo('shipping_', $shippingAddress, $shippingStateCode, $order);
-
-        $firstName = get_user_meta($order->get_user_id(), 'shipping_first_name')[0];
-        $address_1 = get_user_meta($order->get_user_id(), 'shipping_address_1')[0];
-        $address_2 = get_user_meta($order->get_user_id(), 'shipping_address_2')[0];
-        $country = get_user_meta($order->get_user_id(), 'shipping_country')[0];
-        $postcode = get_user_meta($order->get_user_id(), 'shipping_postcode')[0];
-        $email = get_user_meta($order->get_user_id(), 'shipping_email')[0];
-        $phone = get_user_meta($order->get_user_id(), 'shipping_phone')[0];
-        $city = get_user_meta($order->get_user_id(), 'shipping_city')[0];
-        $state = get_user_meta($order->get_user_id(), 'shipping_state')[0];
-
-        $this->assertSame($shippingAddress['first_name'], $firstName);
-
-        $this->assertSame($shippingAddress['address_1'], $address_1);
-
-        $this->assertSame($shippingAddress['address_2'], $address_2);
-
-        $this->assertSame($shippingAddress['country'], $country);
-
-        $this->assertSame($shippingAddress['postcode'], $postcode);
-
-        $this->assertSame($shippingAddress['email'], $email);
-
-        $this->assertSame($shippingAddress['phone'], $phone);
-
-        $this->assertSame($shippingAddress['city'], $city);
-
-        $this->assertSame($shippingStateCode, $state);
-    }
-
-    public function testhandleErrorCase()
-    {
-        global $woocommerce;
-
-        $order = wc_create_order();
-
-        $actual = "An error occured. Please contact administrator for assistance";
         
-        $this->instance->handleErrorCase($order);
+        $razorpayData = array('customer_details' => array('email' => 'abc.xyz@razorpay.com', 'contact' => '18001231272','shipping_address' => $shippingAddress));
+       
+        add_option('woocommerce_razorpay_settings',array('1cc_account_creation'=> 'yes'));
 
-        $reflection = new ReflectionProperty('WC_Razorpay', 'msg');
-        $reflection->setAccessible(true);
-        $msg = $reflection->getValue($this->instance);
+        $this->instance->newUserAccount($razorpayData,$order);
 
-        $this->assertSame($msg['message'], $actual);
+        $current_user = get_user_by( 'email', 'abc.xyz@razorpay.com' );
+        $userID = $current_user->data->ID;
 
-        $this->assertSame($msg['class'],'error');
+        $this->assertNotNull(get_post_meta($order->get_id(), '_customer_user', $userId));
+
+        $this->assertSame('abc.xyz@razorpay.com', get_user_meta($userID,'shipping_email',$email)[0]);
+
+        $this->assertSame('18001231272', get_user_meta($userID,'shipping_phone',$contact)[0]);
+
+        $this->assertSame($shippingAddress->name, get_user_meta($userID, 'shipping_first_name', $shippingAddress->name )[0]);
+
+        $this->assertSame($shippingAddress->line1, get_user_meta($userID, 'shipping_first_name', $shippingAddress->line1 )[0]);
+
+        $this->assertSame($shippingAddress->line2, get_user_meta($userID, 'shipping_first_name', $shippingAddress->line2 )[0]);
+
+        $this->assertSame($shippingAddress->city, get_user_meta($userID, 'shipping_first_name', $shippingAddress->city )[0]);
+
+        $this->assertSame($shippingAddress->country, get_user_meta($userID, 'shipping_first_name', $shippingAddress->country )[0]);
+
+        $this->assertSame($shippingAddress->zipcode, get_user_meta($userID, 'shipping_first_name', $shippingAddress->zipcode )[0]);
+
+        $this->assertSame($shippingAddress->name, get_user_meta($userID, 'billing_first_name', $shippingAddress->name )[0]);
+
+        $this->assertSame($shippingAddress->line1, get_user_meta($userID, 'billing_first_name', $shippingAddress->line1 )[0]);
+
+        $this->assertSame($shippingAddress->line2, get_user_meta($userID, 'billing_first_name', $shippingAddress->line2 )[0]);
+
+        $this->assertSame($shippingAddress->city, get_user_meta($userID, 'billing_first_name', $shippingAddress->city )[0]);
+
+        $this->assertSame($shippingAddress->country, get_user_meta($userID, 'billing_first_name', $shippingAddress->country )[0]);
+
+        $this->assertSame($shippingAddress->zipcode, get_user_meta($userID, 'billing_first_name', $shippingAddress->zipcode )[0]);
+    } 
+
+    public function testverifyOrderAmount()
+    {
+        $order = wc_create_order();
+        
+        $wcOrderId = $order->get_id();
+
+        
+        $this->instance->shouldReceive('getRazorpayApiInstance')->andReturnUsing(
+            function () {
+                return new MockApi('key_id_2', 'key_secret2');
+            });
+
+        set_transient("razorpay_order_id".$wcOrderId, "razorpay_order_id", 18000);
+
+        $razorpayOrderId = get_transient("razorpay_order_id".$wcOrderId);
+
+        $response = $this->instance->verifyOrderAmount($razorpayOrderId,$wcOrderId);
+        
+        $this->assertSame(true,$response);
     }
 }
