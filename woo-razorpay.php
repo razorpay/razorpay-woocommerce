090355a93f93bf561917312ff44d9a640743d2ee
abdulwahidsharief <abdul.sharief@razorpay.com>
2023-02-02 17:16:13 +0530

    removed redundant function

--- woo-razorpay.php
+++ woo-razorpay.php
@@ -1465,11 +1465,6 @@ EOT;
             return new Api($this->getSetting('key_id'), "");
         }
 
-        public function getRazorpayApiPublicInstance()
-        {
-            return new Api($this->getSetting('key_id'), "");
-        }
-
         /**
          * Check for valid razorpay server callback
          * Called once payment is completed using redirect method
