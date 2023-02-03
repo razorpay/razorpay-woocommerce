18b59e4e7927ea072642c62d1bb5e21b8cab749b
garimaprachi <105697320+garimaprachi@users.noreply.github.com>
2023-02-03 19:14:25 +0530

    Update test-affordabilitywidget.php

--- tests/phpunit/tests/test-affordabilitywidget.php
+++ tests/phpunit/tests/test-affordabilitywidget.php
@@ -6,15 +6,6 @@ use Razorpay\MockApi\MockApi;
 
 class Test_AfdWidget extends \PHPUnit_Framework_TestCase
 {
-    protected $backupGlobals = FALSE;
-    
-    public function testgetKeyID()
-    {
-        add_option('woocommerce_razorpay_settings', array('key_id' => 'key_id_2', 'key_secret' => 'key_secret2'));
-
-        $this->assertSame('key_id_2', getKeyId());
-    }
-
     public function testgetPrice()
     {
         global $product;
