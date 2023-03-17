930a0d628b2c5f963319efd6db557f4d703eb4d7
garimaprachi <105697320+garimaprachi@users.noreply.github.com>
2023-03-17 11:49:51 +0530

    Update test-razorpay-route.php

--- tests/phpunit/tests/test-razorpay-route.php
+++ tests/phpunit/tests/test-razorpay-route.php
@@ -321,11 +321,12 @@ class Test_RzpRoute extends \PHPUnit_Framework_TestCase
 
         $_REQUEST = array('s' => 'ABC123');
 
-        $reversalPage = array(
+        $reversalPage = array(0 => array(
             'reversal_id' => '9654',
             'transfer_id' => '1234',
             'amount' => '2500',
             'created_at' => 1677542400
+            )
         );
 
         $this->instance->shouldReceive('getReversalItems')->with(10)->andReturn($reversalPage);
