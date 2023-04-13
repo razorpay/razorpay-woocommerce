4e9508784e20a1f7495b6408a56b37c9a6f9cbbb
poojarazor <pooja.gupta@razorpay.com>
2023-04-13 11:46:00 +0530

    version bumpup 4.5.0

--- woo-razorpay.php
+++ woo-razorpay.php
@@ -3,8 +3,8 @@
  * Plugin Name: Razorpay for WooCommerce
  * Plugin URI: https://razorpay.com
  * Description: Razorpay Payment Gateway Integration for WooCommerce
- * Version: 4.4.3
- * Stable tag: 4.4.3
+ * Version: 4.5.0
+ * Stable tag: 4.5.0
  * Author: Team Razorpay
  * WC tested up to: 6.7.0
  * Author URI: https://razorpay.com
