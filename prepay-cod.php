90208d6115969d5f5fc331c0db7b61b48b848370
Anantha-Perumal <anantha.perumal@razorpay.com>
2023-06-26 20:55:09 +0530

    prepay cod: handling razorpay offers

--- prepay-cod.php
+++ prepay-cod.php
@@ -2,10 +2,10 @@
 
 
 const WC_ORDER_ID                    = 'woocommerce_order_id';
-
 const RAZORPAY_PAYMENT_ID            = 'razorpay_payment_id';
 const RAZORPAY_ORDER_ID              = 'razorpay_order_id';
 const RAZORPAY_SIGNATURE             = 'razorpay_signature';
+const RAZORPAY_OFFER                 = 'razorpay_offer';
 
 function prepayCODOrder(array $payload): WP_REST_Response {
     $wcRazorpay = new WC_Razorpay(false);
@@ -14,6 +14,7 @@ function prepayCODOrder(array $payload): WP_REST_Response {
     $razorpayPaymentId = $payload[RAZORPAY_PAYMENT_ID];
     $razorpayOrderId = $payload[RAZORPAY_ORDER_ID];
     $signature = $payload[RAZORPAY_SIGNATURE];
+    $razorpayOffer = $payload[RAZORPAY_OFFER];
     $attributes = array(
         RAZORPAY_ORDER_ID => $razorpayOrderId,
         RAZORPAY_PAYMENT_ID => $razorpayPaymentId,
@@ -35,20 +36,37 @@ function prepayCODOrder(array $payload): WP_REST_Response {
         return new WP_REST_Response(['code' => 'woocommerce_order_not_in_on_hold_status'], 400);
     }
 
+    if ((isset($payload['coupon']) || $razorpayOffer > 0) && get_option("woocommerce_enable_coupons") === "no")
+    {
+        return new WP_REST_Response(['code' => 'woocommerce_merchant_coupon_feature_disabled'], 400);
+    }
+
     $order->set_status('pending');
     $order->save();
+    $couponInput = [];
 
     if (isset($payload['coupon']))
     {
         $couponKey = $payload['coupon']['code'];
         $amount = $payload['coupon']['amount'];
-        $isSuccess = createCoupon($couponKey, $amount/100);
-        if ($isSuccess === false) {
-			rzpLogError("Prepay COD create coupon error");
-            return new WP_REST_Response(['code' => 'woocommerce_create_new_coupon_error'], 500);
+        $amountInRs = $amount/100;
+        $couponInput[$couponKey] = $amountInRs;
+    }
+    if ($razorpayOffer > 0)
+    {
+        $razorpayOfferInRs = $razorpayOffer/100;
+        $couponKey = 'Razorpay offers_'. $orderId .'(₹'. $razorpayOfferInRs .')';
+        $couponInput[$couponKey] = $razorpayOfferInRs;
+    }
+
+    if (sizeof($couponInput) > 0 )
+    {
+        $error = bulkCouponApply($couponInput, $wcRazorpay, $order);
+        if (sizeof($error) > 0 )
+        {
+            handleCouponFailure($couponInput, $order);
+            return new WP_REST_Response($error, 500);
         }
-        $wcRazorpay->applyCoupon($order, $couponKey, $amount);
-        rzpLogInfo("Coupon details updated for orderId: $orderId, razorpayOrderId: $razorpayOrderId, couponKey: $couponKey");
     }
     $order->set_payment_method($wcRazorpay->id);
     $order->set_payment_method_title($wcRazorpay->title);
@@ -57,6 +75,26 @@ function prepayCODOrder(array $payload): WP_REST_Response {
     $order->save();
     $order->add_order_note("COD Order Converted to Prepaid <br/> Razorpay payment successful <br/>Razorpay Id: $razorpayPaymentId");
     return new WP_REST_Response([], 200);
+}
+
+function handleCouponFailure($couponInput, $order) {
+    foreach($couponInput as $couponCode => $amount) {
+        $order->remove_coupon($couponCode);
+    }
+    $order->set_status('on-hold');
+    $order->save();
+}
+
+function bulkCouponApply($input, $wcRazorpay, $order) : array {
+    foreach($input as $couponCode => $amount) {
+        $isSuccess = createCoupon($couponCode, $amount);
+        if ($isSuccess === false) {
+            rzpLogError("Prepay cod create coupon error, key : " . $couponCode);
+            return['code' => 'woocommerce_create_new_'. $couponCode .'_coupon_error'];
+        }
+        $wcRazorpay->applyCoupon($order, $couponCode, $amount);
+    }
+    return [];
 }
 
 function createCoupon($coupon_code, $amount) : bool {
