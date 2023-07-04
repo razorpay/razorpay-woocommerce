f3a053861ed1e7b628b781b45e56b2bab9f95d72
poojarazor <pooja.gupta@razorpay.com>
2023-07-04 12:04:40 +0530

    change time

--- includes/cron/plugin-fetch.php
+++ includes/cron/plugin-fetch.php
@@ -9,25 +9,26 @@ add_action('one_cc_plugin_sync_cron', 'one_cc_plugin_sync_cron_exce');
 
 function one_cc_plugin_sync_cron_exce() 
 {
+    rzpLogInfo('Execute one_cc_plugin_sync_cron successful');
 
-	$siteUrl = get_option('siteurl');
+    $siteUrl = get_option('siteurl');
 
     //get all plugin details
-	$data = get_plugins();
+    $data = get_plugins();
 
     //remove additional fields
     foreach(array_keys($data) as $key) {
          unset($data[$key]['DomainPath'], $data[$key]['TextDomain'], $data[$key]['Description'], $data[$key]['AuthorURI']);
     }
 
-	$pluginData = ["url" => $siteUrl, "platform" => "woocommerce", "plugin_info" => $data];
+    $pluginData = ["url" => $siteUrl, "platform" => "woocommerce", "plugin_info" => $data];
 
-	try
+    try
     {
-    	$paymentSettings = get_option('woocommerce_razorpay_settings');
+        $paymentSettings = get_option('woocommerce_razorpay_settings');
 
-    	$api = new Api($paymentSettings['key_id'], $paymentSettings['key_secret']);
-    	$response = $api->request->request('POST', RZP_1CC_PLUGIN_FETCH, $pluginData);
+        $api = new Api($paymentSettings['key_id'], $paymentSettings['key_secret']);
+        $response = $api->request->request('POST', RZP_1CC_PLUGIN_FETCH, $pluginData);
     }
     catch (Exception $e)
     {
@@ -38,13 +39,13 @@ function one_cc_plugin_sync_cron_exce()
 
 function syncPluginFetchCron(){
 
-    $startTime = strtotime('+15 days 2:00:00');
-    $endTime = strtotime('+15 days 2:15:00');
+    $startTime = strtotime('tomorrow 20:30');
+    $endTime =  strtotime('tomorrow 20:45');
     $randomTime = mt_rand($startTime, $endTime);
 
     try
     {
-        createPluginFetchCron('one_cc_plugin_sync_cron', $randomTime , 'daily');
+        createPluginFetchCron('one_cc_plugin_sync_cron', $randomTime , 'fifteendays');
         rzpLogInfo('create one_cc_plugin_sync_cron successful');
     }
     catch (Exception $e)
