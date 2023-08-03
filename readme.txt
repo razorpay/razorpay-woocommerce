02f3d80829f554a7083ae85b972191583a1e0cf2
poojarazor <pooja.gupta@razorpay.com>
2023-08-03 16:11:31 +0530

    changes in version name

--- readme.txt
+++ readme.txt
@@ -41,7 +41,7 @@ This is compatible with WooCommerce>=2.4, including the new 3.0 release. It has 
 
 == Changelog ==
 
-= 4.6.0 =
+= 4.5.4 =
 * Add compatibility with WooCommerce 7.9
 * Bug fix, city restriction for smart cod
 
--- woo-razorpay.php
+++ woo-razorpay.php
@@ -3,8 +3,8 @@
  * Plugin Name: Razorpay for WooCommerce
  * Plugin URI: https://razorpay.com
  * Description: Razorpay Payment Gateway Integration for WooCommerce
- * Version: 4.6.0
- * Stable tag: 4.6.0
+ * Version: 4.5.4
+ * Stable tag: 4.5.4
  * Author: Team Razorpay
  * WC tested up to: 7.9.0
  * Author URI: https://razorpay.com
