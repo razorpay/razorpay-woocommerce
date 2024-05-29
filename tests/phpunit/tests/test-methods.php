36b84d31437146c3eed89a06ba9b495fdefeed89
rohitcbr <rohit.chakraborty@razorpay.com>
2024-05-29 14:37:33 +0530

    Update test-methods.php

--- tests/phpunit/tests/test-methods.php
+++ tests/phpunit/tests/test-methods.php
@@ -102,7 +102,7 @@ class Test_Class_Fuctions extends WP_UnitTestCase
         $orderId = $order->get_id();
 
         $this->instance->shouldReceive('autoEnableWebhook');
-        $razorpayOrderId=$this->instance->shouldReceive('createOrGetRazorpayOrderId')->andReturn('order_test');
+        $razorpayOrderId = $this->instance->shouldReceive('createOrGetRazorpayOrderId')->andReturn('order_test');
 
         $this->instance->shouldReceive('getRazorpayApiPublicInstance')->andReturnUsing(function () {
             return new MockApi('key_id', '');
